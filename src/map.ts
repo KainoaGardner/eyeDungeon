db75ca8a1bcc420361b422eda82a05d7d504453c
Kainoa Gardner <kainoagardner123@gmail.com>
2025-02-05 21:27:06 -0800

    temp 2

--- src/map.ts
+++ src/map.ts
@@ -1,7 +1,32 @@
 export class Map {
   map: number[][];
+  size: number;
 
-  constructor(map: number[][]) {
+  constructor(map: number[][], canvasHeight: number) {
     this.map = map;
+    this.size = canvasHeight / map.length;
+    console.log(this.size);
+  }
+
+  draw(c: any, scale: number) {
+    const rows = this.map.length;
+    const cols = this.map[0].length;
+
+    c.fillStyle = "#7f8c8d";
+    c.fillRect(0, 0, rows * this.size * scale, cols * this.size * scale);
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        if (this.map[i][j] !== 0) {
+          c.fillStyle = "black";
+          c.fillRect(
+            this.size * j * scale,
+            this.size * i * scale,
+            this.size * scale,
+            this.size * scale,
+          );
+        }
+      }
+    }
   }
 }
--- src/player.ts
+++ src/player.ts
@@ -2,60 +2,97 @@ export class Player {
   x: number;
   y: number;
 
-  direction: number = 0;
+  pos: number[] = [22, 12];
+  dir: number[] = [-1, 0];
+  planeX: number = 0;
+  planeY: number = 1;
 
+  radius: number;
+  direction: number = 0;
   turnSpeed: number = 1;
 
   speed: number;
 
-  constructor(x: number, y: number, speed: number) {
+  constructor(x: number, y: number, radius: number, speed: number) {
     this.x = x;
     this.y = y;
+    this.radius = radius;
     this.speed = speed;
   }
 
-  draw(c: any): void {
-    const angleRad = (this.direction * Math.PI) / 180;
-    c.beginPath();
-    c.arc(this.x, this.y, 30, 0, 2 * Math.PI);
-    c.fillStyle = "red";
-    c.fill();
-
-    c.beginPath();
-    c.arc(this.x, this.y, 30, angleRad - Math.PI / 2, angleRad + Math.PI / 2);
-    c.lineWidth = 5;
-    c.strokeStyle = "black";
-    c.stroke();
+  cast(canvasWidth: number): void {
+    for (let x = 0; x < canvasWidth; x++) {}
   }
 
-  update(keyMap: Map<string, boolean>, canvas: any): void {
-    if (keyMap.get("ArrowRight")) {
-      this.direction += this.turnSpeed;
-    }
-    if (keyMap.get("ArrowLeft")) {
-      this.direction -= this.turnSpeed;
-    }
+  draw(c: any, scale: number): void {
+    // const angleRad = (this.direction * Math.PI) / 180;
+    // c.beginPath();
+    // c.arc(this.x * scale, this.y * scale, this.radius * scale, 0, 2 * Math.PI);
+    // c.fillStyle = "red";
+    // c.fill();
+    //
+    // c.beginPath();
+    // c.arc(
+    //   this.x * scale,
+    //   this.y * scale,
+    //   this.radius * scale,
+    //   angleRad - Math.PI / 2,
+    //   angleRad + Math.PI / 2,
+    // );
+    // c.lineWidth = 5;
+    // c.strokeStyle = "black";
+    // c.stroke();
+  }
 
-    if (keyMap.get("ArrowUp")) {
-      this.x += this.speed * Math.cos((this.direction * Math.PI) / 180);
-      this.y += this.speed * Math.sin((this.direction * Math.PI) / 180);
-    }
-    if (keyMap.get("ArrowDown")) {
-      this.x -= this.speed * Math.cos((this.direction * Math.PI) / 180);
-      this.y -= this.speed * Math.sin((this.direction * Math.PI) / 180);
-    }
-
-    if (this.x > canvas.width - 30) {
-      this.x = canvas.width - 30;
-    }
-    if (this.x < 30) {
-      this.x = 30;
-    }
-    if (this.y > canvas.height - 30) {
-      this.y = canvas.height - 30;
-    }
-    if (this.y < 30) {
-      this.y = 30;
-    }
+  update(
+    keyMap: Map<string, boolean>,
+    map: number[][],
+    blockSize: number,
+  ): void {
+    // if (keyMap.get("ArrowRight")) {
+    //   this.direction += this.turnSpeed;
+    // }
+    // if (keyMap.get("ArrowLeft")) {
+    //   this.direction -= this.turnSpeed;
+    // }
+    //
+    // if (keyMap.get("ArrowUp")) {
+    //   const nx =
+    //     this.x + this.speed * Math.cos((this.direction * Math.PI) / 180);
+    //   const ny =
+    //     this.y + this.speed * Math.sin((this.direction * Math.PI) / 180);
+    //
+    //   const xBlock = Math.floor(nx / blockSize);
+    //   const yBlock = Math.floor(ny / blockSize);
+    //   if (
+    //     xBlock >= map[0].length ||
+    //     yBlock >= map.length ||
+    //     (xBlock < map[0].length &&
+    //       yBlock < map.length &&
+    //       map[yBlock][xBlock] === 0)
+    //   ) {
+    //     this.x = nx;
+    //     this.y = ny;
+    //   }
+    // }
+    // if (keyMap.get("ArrowDown")) {
+    //   const nx =
+    //     this.x - this.speed * Math.cos((this.direction * Math.PI) / 180);
+    //   const ny =
+    //     this.y - this.speed * Math.sin((this.direction * Math.PI) / 180);
+    //
+    //   const xBlock = Math.floor(nx / blockSize);
+    //   const yBlock = Math.floor(ny / blockSize);
+    //   if (
+    //     xBlock >= map[0].length ||
+    //     yBlock >= map.length ||
+    //     (xBlock < map[0].length &&
+    //       yBlock < map.length &&
+    //       map[yBlock][xBlock] === 0)
+    //   ) {
+    //     this.x = nx;
+    //     this.y = ny;
+    //   }
+    // }
   }
 }
